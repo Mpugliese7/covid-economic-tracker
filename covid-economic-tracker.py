597aca3a637b00b217ffb423540c71310b090e24
Mpugliese7 <65572796+Mpugliese7@users.noreply.github.com>
2020-06-28 16:54:20 -0400

    Adding dates to all-time highs/lows

--- covid-economic-tracker.py
+++ covid-economic-tracker.py
@@ -48,6 +48,14 @@ for v in parsed_response["observations"]:
 all_time_high = max(all_values)
 all_time_low = min(all_values)
 
+matching_dates_low = [v for v in parsed_response["observations"] if v["value"] == str(all_time_low)]
+matching_date_low = matching_dates_low[0]
+all_time_low_date = matching_date_low["date"]
+
+matching_dates_high = [v for v in parsed_response["observations"] if v["value"] == str(all_time_high)]
+matching_date_high = matching_dates_high[0]
+all_time_high_date = matching_date_high["date"]
+
 # Getting the state's pre-COVID-19 unemployment rate
 
 pre_covid_date = "2020-02-01"
@@ -76,8 +84,8 @@ print("The " + str.upper(state) + " Labor Market During COVID-19 Pandemic")
 print("----------------------------------------------------------------------")
 print("Current Unemployment Rate: " + str(last_value) + "%")
 print("February 2020 Unemployment Rate " + str(pre_covid_level) + "%")
-print("All-Time High Unemployment Rate: " + str(all_time_high) + "%")
-print("All-Time Low Unemployment Rate: " + str(all_time_low) + "%")
+print(f"All-Time High Unemployment Rate ({all_time_high_date}): {str(all_time_high)}%")
+print(f"All-Time Low Unemployment Rate ({all_time_low_date}): {str(all_time_low)}%")
 print("Current Unemployment Rate for the United States: " + str(last_value_us) + "%")
 print("Difference between " + str.upper(state) + " and the US: " + str(UR_difference) + "ppts")
 print("----------------------------------------------------------------------")
@@ -89,33 +97,33 @@ print("----------------------------------------------------------------------")
 
 # Data Visualization 1 REFERENCE: https://plotly.com/python/time-series/
 
-import plotly.express as px
+# import plotly.express as px
 
-all_val = []
-a = -1
-for w in parsed_response["observations"]:
-    a = a + 1
-    valuec = float(parsed_response["observations"][a]["value"])
-    all_val.append(valuec)
-values = all_val
-all_date = []
-c = -1
-for y in parsed_response["observations"]:
-    c += 1
-    val_date = parsed_response["observations"][c]["date"]
-    all_date.append(val_date)
-dates = all_date 
+# all_val = []
+# a = -1
+# for w in parsed_response["observations"]:
+#     a = a + 1
+#     valuec = float(parsed_response["observations"][a]["value"])
+#     all_val.append(valuec)
+# values = all_val
+# all_date = []
+# c = -1
+# for y in parsed_response["observations"]:
+#     c += 1
+#     val_date = parsed_response["observations"][c]["date"]
+#     all_date.append(val_date)
+# dates = all_date 
 
 
-fig = px.line(x=dates, y=values, title='Unemployment Rate Time Series')
+# fig = px.line(x=dates, y=values, title='Unemployment Rate Time Series')
 
-fig.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(count=1, label="YTD", step="year", stepmode="todate"),
-            dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")])))
-plotly.offline.plot(fig)
+# fig.update_xaxes(
+#     rangeslider_visible=True,
+#     rangeselector=dict(
+#         buttons=list([
+#             dict(count=1, label="1m", step="month", stepmode="backward"),
+#             dict(count=6, label="6m", step="month", stepmode="backward"),
+#             dict(count=1, label="YTD", step="year", stepmode="todate"),
+#             dict(count=1, label="1y", step="year", stepmode="backward"),
+#             dict(step="all")])))
+# plotly.offline.plot(fig)
